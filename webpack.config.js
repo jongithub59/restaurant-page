97412e9d459afdc15532a56fd4a2613819e7d80d
jongithub59 <jdelrealj01@gmail.com>
2024-01-12 15:47:00 -0600

    added menu and contact pages

--- webpack.config.js
+++ webpack.config.js
@@ -16,10 +16,12 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        outputPath: "pics"
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
+        outputPath: "fonts"
       },
     ],
   },
